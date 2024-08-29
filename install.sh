22b6b5d686a7d3b4d782abeb1c70a2baf9ecf86b
Fabian Offert <fabian@zentralwerkstatt.org>
2024-08-29 08:42:36 -0700

    Hotfix: updated PyTorch GPU switch

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-conda install --yes -c pytorch pytorch torchvision torchaudio ${1:-cpuonly}
+conda install --yes -c pytorch -c nvidia pytorch torchvision torchaudio ${1:-cpuonly}
 conda install --yes -c conda-forge flask flask-cors flask-wtf flask-login flask-migrate flask-sqlalchemy email-validator nodejs
 conda install --yes requests tqdm scikit-learn h5py gunicorn dill
 pip install annoy bootstrap-flask torchray umap-learn
