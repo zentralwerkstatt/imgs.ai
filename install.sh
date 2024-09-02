60c7cd4c815c499efd7f4d25949f42f73c29f210
zentralwerkstatt <fabian@zentralwerkstatt.org>
2024-09-03 01:36:10 +0200

    Hotfix: install markdown

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 conda install --yes -c pytorch -c nvidia pytorch torchvision torchaudio ${1:-cpuonly}
 conda install --yes -c conda-forge flask flask-cors flask-wtf flask-login flask-migrate flask-sqlalchemy email-validator nodejs
-conda install --yes requests tqdm scikit-learn h5py gunicorn dill gdown
+conda install --yes requests tqdm scikit-learn h5py gunicorn dill gdown markdown
 pip install annoy bootstrap-flask
 pip install git+https://github.com/openai/CLIP.git
 cd app/static
