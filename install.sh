fb2abed6f30b65db75ca8486c9d2c07765ac89c4
zentralwerkstatt <fabian@zentralwerkstatt.org>
2025-01-13 07:12:41 +0100

    Hotfix: new Drive link to default model

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ conda install --yes requests tqdm scikit-learn h5py gunicorn dill gdown markdown
 pip install annoy
 pip install git+https://github.com/openai/CLIP.git
 cd ../../models/public
-gdown https://drive.google.com/uc?id=1jVEjmKwv0wwTX2vt7WzAYF8F59_Tsg5s # FIXME: Replace with new link
+gdown https://drive.google.com/uc?id=1XFPI_WJJk6MCZ8CT7D4UaUUJWGMEu1IP
 unzip rma.zip
 rm rma.zip
 mv rma Rijksmuseum
